b686441a9bb40e3145307c6786eea1a3bc3ed529
Rishu Gupta <guptarishu04@gmail.com>
2019-03-21 20:59:54 +0530

    textfield,image is displayed

---
+++ src/components/image-results/ImageResults.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import {GridList,GridTile} from 'material-ui/GridList';
+import IconButton from 'material-ui/IconButton';
+import ZoomIn from 'material-ui/svg-icons/action/zoom-in';
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
+
+class ImageResults extends Component {
+  render() {
+      let imageListContent;
+        const {images}=this.props;
+        if(images){
+imageListContent=(
+    <GridList cols={3}>
+        {images.map(img=>(
+            <GridTile
+            title={img.tags}
+            key={img.id}
+            subtitle={
+                <span>
+                by <strong>{img.user}</strong>
+                </span>
+            }
+            actionIcon={
+                <IconButton>
+                <ZoomIn color="white"/>
+                </IconButton>
+            }
+            >
+            <img src={img.largeImageURL} alt=""/>
+            </GridTile>
+            
+        ))}     
+    </GridList>
+)
+      }else{
+          imageListContent=null;
+      }
+    return (
+      <div>
+        {imageListContent}
+      </div>
+    )
+  }
+}
+// ImageResults.PropTypes={
+//     images:PropTypes.array.isRequired
+// }
+export default ImageResults;
--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import TextField from 'material-ui/TextField';
 import SelectField from 'material-ui/SelectField';
+import { MenuItem } from 'material-ui';
+import axios from 'axios';
+import ImageResults from '../image-results/ImageResults';
+
  class Search extends Component {
      state={
          searchText:'',
@@ -8,11 +12,46 @@ import SelectField from 'material-ui/SelectField';
     apiUrl:'https://pixabay.com/api',
     apiKey:'11948920-12ddc9795adc0f5fe13acbdf3',
     images:[]
-      }
+      };
+      onTextChange=(e)=>{
+          this.setState({[e.target.name]:e.target.value},()=>
+         {
+              axios.get(`${this.state.apiUrl}/?key=${this.state.apiKey}&q=${this.state.searchText}&image_type=photo&
+              per_page=${this.state.amount}&safesearch=true`)
+              .then(res=>this.setState({images:res.data.hits}))
+              .catch(err=>console.log(err));
+          });
+      };
+
+onAmountChange=(e,index,value)=>this.setState({amount:value});
+ 
+
   render() {
+      console.log(this.state.images);
     return (
       <div>
-        
+        <TextField
+        name="searchText"
+        value={this.state.searchText}
+        onChange={this.onTextChange}
+        floatingLabelText="Search For Images"
+        fullWidth={true}
+        />
+        <br/>
+        <SelectField
+        name="amount"
+        floatingLabelText="Amount"
+        value={this.state.amount}
+        onChange={this.onAmountChange}
+        >
+        <MenuItem value={5} primaryText="5"/>
+        <MenuItem value={10} primaryText="10"/>
+        <MenuItem value={15} primaryText="15"/>
+        <MenuItem value={25} primaryText="25"/>
+        <MenuItem value={50} primaryText="50 "/>
+        </SelectField>
+        <br/>
+        {this.state.images.length > 0 ? (<ImageResults images={this.state.images}/>):null}
       </div>
     )
   }
