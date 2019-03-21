83d3a5badbc153b10c7c0d15b289bb4e3a42b9ac
Rishu Gupta <guptarishu04@gmail.com>
2019-03-21 22:13:10 +0530

    finally completed

--- src/components/image-results/ImageResults.js
+++ src/components/image-results/ImageResults.js
@@ -1,12 +1,26 @@
+
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import {GridList,GridTile} from 'material-ui/GridList';
+import { GridList, GridTile } from 'material-ui/GridList';
 import IconButton from 'material-ui/IconButton';
 import ZoomIn from 'material-ui/svg-icons/action/zoom-in';
 import Dialog from 'material-ui/Dialog';
 import FlatButton from 'material-ui/FlatButton';
 
 class ImageResults extends Component {
+    state = {
+        open: false,
+        currentImg: ''
+      };
+    
+      handleOpen = img => {
+        this.setState({ open: true, currentImg: img });
+      };
+    
+      handleClose = () => {
+        this.setState({ open: false });
+      };
+
   render() {
       let imageListContent;
         const {images}=this.props;
@@ -23,7 +37,7 @@ imageListContent=(
                 </span>
             }
             actionIcon={
-                <IconButton>
+                <IconButton onClick={() => this.handleOpen(img.largeImageURL)}>
                 <ZoomIn color="white"/>
                 </IconButton>
             }
@@ -33,17 +47,32 @@ imageListContent=(
             
         ))}     
     </GridList>
-)
+);
       }else{
           imageListContent=null;
       }
+      const actions = [
+        <FlatButton label="Close" primary={true} onClick={this.handleClose} />
+      ];
     return (
       <div>
         {imageListContent}
+        <Dialog
+        actions={actions}
+        modal={false}
+        open={this.state.open}
+        onRequestClose={this.handleClose}
+      >
+        <img src={this.state.currentImg} alt="" style={{ width: '100%' }} />
+      </Dialog>
       </div>
-    )
+    );
   }
 }
+ImageResults.propTypes = {
+    images: PropTypes.array.isRequired
+  };
+  
 // ImageResults.PropTypes={
 //     images:PropTypes.array.isRequired
 // }
