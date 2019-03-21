4ed44ff23d5fe99657870dbad8de6fcebad58f95
Rishu Gupta <guptarishu04@gmail.com>
2019-03-21 18:32:03 +0530

    Navbar intialised

---
+++ src/components/search/Search.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import TextField from 'material-ui/TextField';
+import SelectField from 'material-ui/SelectField';
+ class Search extends Component {
+     state={
+         searchText:'',
+        amount:15,
+    apiUrl:'https://pixabay.com/api',
+    apiKey:'11948920-12ddc9795adc0f5fe13acbdf3',
+    images:[]
+      }
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+export default Search;
