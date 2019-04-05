68b011c827a932c45783e9b2d6667fb685509752
Rishu Gupta <guptarishu04@gmail.com>
2019-04-05 22:49:41 +0530

    Updates

---
+++ precache-manifest.46e248f789839ea0254ba763d30cbaaf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d3aa5f40f9eaacf4715e",
+    "url": "/static/js/main.e74bc906.chunk.js"
+  },
+  {
+    "revision": "9066108b4b422d0074ea",
+    "url": "/static/js/2.e3e43bdf.chunk.js"
+  },
+  {
+    "revision": "d3aa5f40f9eaacf4715e",
+    "url": "/static/css/main.542bcf1e.chunk.css"
+  },
+  {
+    "revision": "808073efa4cd0458dc966a55db99670c",
+    "url": "/index.html"
+  }
+];
